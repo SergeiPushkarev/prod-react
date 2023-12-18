7a20451323d717328419f824947e6e1ba3792078
SergeiPushkarev <87870860+SergeiPushkarev@users.noreply.github.com>
2023-12-18 12:36:56 +0300

    install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest:true,
+    jest: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:i18next/recommended'
-  ],
+  extends: ['plugin:react/recommended', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'i18next',
-  ],
+  plugins: ['react', '@typescript-eslint', 'i18next'],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
     indent: [2, 2],
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.tsx']
+    }],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'warn',
@@ -37,9 +32,12 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
-    "i18next/no-literal-string": ['error',{ markupOnly:true, ignoreAttribute:["data-testid", 'to'] }],
+    "i18next/no-literal-string": ['error', {
+      markupOnly: true,
+      ignoreAttribute: ["data-testid", 'to']
+    }]
   },
   globals: {
-    __IS_DEV__: true,
-  },
+    __IS_DEV__: true
+  }
 };
